8d33bbd0d830bd2c027807cd1400b97d2de2e341
illl48 <mruncanny@gmail.com>
2017-03-15 13:24:46 -0700

    add example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+//import createLogger from 'redux-logger'
+import reducer from './reducers'
+import App from './containers/App'
 
-ReactDOM.render(
-  <App />,
+const middleware = [ thunk ]
+/*
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger())
+}
+*/
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware)
+)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
