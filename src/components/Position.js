c4bcebf3a5aa55dd1ee94ef3be6e2df104af0ebb
illl48 <mruncanny@gmail.com>
2017-04-25 15:11:02 -0700

    fix todays return

--- src/components/Position.js
+++ src/components/Position.js
@@ -3,28 +3,37 @@ import '../styles/Position.css'
 import { printDateOnly } from '../utils'
 
 const Position = ({ position, quote }) => {
-  const { quantity, average_buy_price } = position;
+  const quantity = Number(position.quantity);
+  const average_buy_price = Number(position.average_buy_price);
+  const intraday_average_buy_price = Number(position.intraday_average_buy_price);
+  const intraday_quantity = Number(position.intraday_quantity);
+  const previous_close = Number(quote.previous_close);
+
   const lastPrice = (quote.last_extended_hours_trade_price)? quote.last_extended_hours_trade_price : quote.last_trade_price;
   const equityValue = (lastPrice*quantity).toFixed(2);
-  let totalReturn = (lastPrice - Number(average_buy_price))*quantity;
+  let totalReturn = (lastPrice - average_buy_price)*quantity;
   if(totalReturn < 0) {
     totalReturn = "-US$"+(-1)*totalReturn.toFixed(2)
   }
   else {
     totalReturn = "US$"+totalReturn.toFixed(2)
   }
+  const totalReturnPercentage = ( ( lastPrice - average_buy_price ) / average_buy_price*100 ).toFixed(2);
 
-  const totalReturnPercentage = ((lastPrice - Number(average_buy_price))/Number(average_buy_price)*100).toFixed(2);
   let todaysReturn = 0;
   let todaysReturnPercentage = 0;
-  if(printDateOnly(position.created_at) === printDateOnly(new Date().toISOString()) || printDateOnly(position.updated_at) === printDateOnly(new Date().toISOString()) ) {
-    console.log("same day");
-    todaysReturn = (lastPrice - Number(average_buy_price))*quantity;
-    todaysReturnPercentage = ((lastPrice - Number(average_buy_price))/Number(average_buy_price)*100).toFixed(2);
+  if( intraday_average_buy_price > 0 && intraday_quantity > 0 ) {
+    let TodayOnlyReturn = (lastPrice - intraday_average_buy_price) * intraday_quantity;
+    let notTodayQuantity = quantity - intraday_quantity;
+    let notTodayAvgPrice = (( average_buy_price * quantity) - (intraday_average_buy_price*intraday_quantity))/notTodayQuantity
+
+    //let notTodayPrice
+    todaysReturn = (lastPrice - previous_close) * notTodayQuantity + TodayOnlyReturn;
+    todaysReturnPercentage =  (( todaysReturn / ( (notTodayAvgPrice*notTodayQuantity) + (intraday_average_buy_price*intraday_quantity) ) )*100).toFixed(2);
   }
   else {
-    todaysReturn = (lastPrice - Number(quote.previous_close))*quantity;
-    todaysReturnPercentage = ((lastPrice - Number(quote.previous_close))/Number(quote.previous_close)*100).toFixed(2);
+    todaysReturn = (lastPrice - previous_close)*quantity;
+    todaysReturnPercentage = ((lastPrice - previous_close)/previous_close*100).toFixed(2);
   }
 
   if(todaysReturn < 0) {
